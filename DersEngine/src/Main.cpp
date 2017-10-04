ae7a482edd5f4c0a5a464d553e7e78f5586cf245
Anders Eriksson <ders@stenkrossstudios.com>
2017-10-04 23:16:03 +0200

    initial commit

---
+++ DersEngine/src/Main.cpp
@@ -0,0 +1,41 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+		
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize glew!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
