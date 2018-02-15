b25a6fd67082d02620ce8a1bc4dadd3e0e359149
Anders Eriksson <anders__e92@hotmail.com>
2018-02-15 22:36:48 +0100

    Further extension of math library
    Implemented slerp and lookat and more utility funcitons like clamp and isclosetozero

--- DersEngine/Source/MathLibrary/MathLibrary.cpp
+++ DersEngine/Source/MathLibrary/MathLibrary.cpp
@@ -8,19 +8,48 @@ namespace DersEngine
 {
 	namespace Maths
 	{
-		float Maths::ToRadians(float degrees)
+		float ToRadians(float degrees)
 		{
 			return degrees * PI / 180.0f;
 		}
 
-		float Maths::ToDegrees(float radians)
+		float ToDegrees(float radians)
 		{
 			return radians * 180.0f / PI;
 		}
 
+		float Clamp(float value, float min, float max)
+		{
+			return value <= min ? min :
+				value >= max ? max : value;
+		}
+
+		float Max(float value1, float value2)
+		{
+			return value1 > value2 ? value1 : value2;
+		}
+
+		float Cos(float angleInRadians)
+		{
+			float r = cos(angleInRadians);
+			return IsCloseToZero(r) ? 0.0f : r;
+		}
+
+		float Sin(float angleInRadians)
+		{
+			float r = sin(angleInRadians);
+			return IsCloseToZero(r) ? 0.0f : r;
+		}
+
+		float Tan(float angleInRadians)
+		{
+			float r = tan(angleInRadians);
+			return IsCloseToZero(r) ? 0.0f : r;
+		}
+
 		/* 2D VECTOR OPERATIONS */
 
-		float Maths::Distance(const Vector2f& vector1, const Vector2f& vector2)
+		float Distance(const Vector2f& vector1, const Vector2f& vector2)
 		{
 			float xDiff = vector1.x - vector2.x;
 			float yDiff = vector1.y - vector2.y;
@@ -28,19 +57,19 @@ namespace DersEngine
 			return sqrt(xDiff * xDiff + yDiff * yDiff);
 		}
 
-		float Maths::Dot(const Vector2f& vector1, const Vector2f& vector2)
+		float Dot(const Vector2f& vector1, const Vector2f& vector2)
 		{
 			return vector1.x * vector2.x + vector1.y * vector2.y;
 		}
 
-		Vector2f Maths::Lerp(const Vector2f& start, const Vector2f& target, float alpha)
+		Vector2f Lerp(const Vector2f& start, const Vector2f& target, float alpha)
 		{
 			return start + (target - start) * alpha;
 		}
 
 		/* 3D VECTOR OPERATIONS */
 
-		float Maths::Distance(const Vector3f& vector1, const Vector3f& vector2)
+		float Distance(const Vector3f& vector1, const Vector3f& vector2)
 		{
 			float xDiff = vector1.x - vector2.x;
 			float yDiff = vector1.y - vector2.y;
@@ -49,12 +78,12 @@ namespace DersEngine
 			return sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
 		}
 
-		float Maths::Dot(const Vector3f& vector1, const Vector3f& vector2)
+		float Dot(const Vector3f& vector1, const Vector3f& vector2)
 		{
 			return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
 		}
 
-		Vector3f Maths::Cross(const Vector3f& vector1, const Vector3f& vector2)
+		Vector3f Cross(const Vector3f& vector1, const Vector3f& vector2)
 		{
 			return
 			{
@@ -64,12 +93,12 @@ namespace DersEngine
 			};
 		}
 
-		Vector3f Maths::Lerp(const Vector3f& start, const Vector3f& target, float alpha)
+		Vector3f Lerp(const Vector3f& start, const Vector3f& target, float alpha)
 		{
 			return start + (target - start) * alpha;
 		}
 
-		Vector3f Maths::Rotate(const Vector3f& vector, const Quaternion& rotation)
+		Vector3f Rotate(const Vector3f& vector, const Quaternion& rotation)
 		{
 			Quaternion quat = rotation * vector * rotation.Conjugate();
 			return { quat.x, quat.y, quat.z };
@@ -77,55 +106,82 @@ namespace DersEngine
 
 		/* QUATERNION OPERATIONS */
 
-		float Maths::Dot(const Quaternion& quat1, const Quaternion& quat2)
+		float Dot(const Quaternion& quat1, const Quaternion& quat2)
 		{
 			return quat1.x * quat2.x + quat1.y * quat2.y + quat1.z * quat2.z + quat1.w * quat2.w;
 		}
 
-		Quaternion Maths::MakeQuaternion(const Vector3f& axis, float angle)
+		Quaternion LookAt(const Vector3f& start, const Vector3f& target)
 		{
-			Quaternion quat;
+			Vector3f directionToTarget = (target - start).Normalize();
 
-			float rad = ToRadians(angle);
+			float dot = Dot(Vector3f::ZAxis(), directionToTarget);
 
-			float sinHalfAngle = sin(rad / 2.0f);
-			float cosHalfAngle = cos(rad / 2.0f);
+			if (abs(dot + 1.0f) < 0.000001f)
+			{
+				return { Vector3f::YAxis().x, Vector3f::YAxis().y, Vector3f::YAxis().z, PI };
+			}
 
-			quat.x = axis.x * sinHalfAngle;
-			quat.y = axis.y * sinHalfAngle;
-			quat.z = axis.z * sinHalfAngle;
-			quat.w = cosHalfAngle;
+			if(abs(dot - 1.0f) < 0.000001f)
+			{
+				return Quaternion().Identity();
+			}
 
-			return quat;
+			float angle = acosf(dot);
+			Vector3f rotationAxis = Cross(Vector3f::ZAxis(), directionToTarget).Normalize();
+
+			return Quaternion(rotationAxis, ToDegrees(angle));
 		}
 
-		Quaternion Maths::ToQuaternion(float roll, float pitch, float yaw)
+		Quaternion Slerp(const Quaternion& quat1, Quaternion quat2, float alpha)
 		{
-			float cy = cos(yaw * 0.5);
-			float sy = sin(yaw * 0.5);
-			float cr = cos(roll * 0.5);
-			float sr = sin(roll * 0.5);
-			float cp = cos(pitch * 0.5);
-			float sp = sin(pitch * 0.5);
+			float flCosOmega = quat1.w * quat2.w + Dot(quat2.GetXYZ(), quat1.GetXYZ());
 
-			return 
-			{   
-				cy * sr * cp - sy * cr * sp,
-				cy * cr * sp + sy * sr * cp,
-				sy * cr * cp - cy * sr * sp,
-				cy * cr * cp + sy * sr * sp
-			};
+			if (flCosOmega < 0)
+			{
+				quat2.w = -quat2.w;
+				quat2.x = -quat2.x;
+				quat2.y = -quat2.y;
+				quat2.z = -quat2.z;
+
+				flCosOmega = -flCosOmega;
+			}
+
+			float k0, k1;
+
+			if (flCosOmega > 0.9999f)
+			{
+				k0 = 1 - alpha;
+				k1 = alpha;
+			}
+			else
+			{
+				float flSinOmega = sqrt(1 - flCosOmega * flCosOmega);
+				float flOmega = atan2(flSinOmega, flCosOmega);
+				float flOneOverSinOmega = 1 / flSinOmega;
+
+				k0 = sin((1 - alpha) * flOmega) * flOneOverSinOmega;
+				k1 = sin(alpha * flOmega) * flOneOverSinOmega;
+			}
+
+			Quaternion result;
+
+			result.w = quat1.w * k0 + quat2.w * k1;
+			result.x = quat1.x * k0 + quat2.x * k1;
+			result.y = quat1.y * k0 + quat2.y * k1;
+			result.z = quat1.z * k0 + quat2.z * k1;
+
+			return result;
 		}
 
-		Vector3f Maths::ToEuler(const Quaternion& quat)
+		Quaternion FromEuler(float pitch, float yaw, float roll)
 		{
-			quat.Normalize();
-			return 
-			{ 
-				atan2(2 * quat.x * quat.w - 2 * quat.y * quat.z, 1 - 2 * quat.x * quat.x - 2 * quat.z * quat.z), 
-				atan2(2 * quat.y * quat.w - 2 * quat.x * quat.z, 1 - 2 * quat.y * quat.y - 2 * quat.z * quat.z),
-				asin(2 * quat.x * quat.y + 2 * quat.z * quat.w) 
-			};
+			return Quaternion();
+		}
+
+		Vector3f ToEuler(const Quaternion& q1)
+		{
+			return Vector3f();
 		}
 
 		Matrix4f ToRotationMatrix(const Quaternion& quat)
@@ -157,7 +213,7 @@ namespace DersEngine
 
 		/* 4x4 MATRIX OPERATIONS */
 
-		Matrix4f Maths::Translate(Matrix4f& mat, float x, float y, float z)
+		Matrix4f Translate(Matrix4f& mat, float x, float y, float z)
 		{
 			//col + row * 4
 			mat.Identity();
@@ -169,7 +225,7 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::Scale(Matrix4f& mat, float x, float y, float z)
+		Matrix4f Scale(Matrix4f& mat, float x, float y, float z)
 		{
 			//col + row * 4
 			mat.Identity();
@@ -181,17 +237,17 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::Rotate(Matrix4f& mat, const Vector3f& axis, float angle)
+		Matrix4f Rotate(Matrix4f& mat, const Vector3f& axis, float angle)
 		{
 			return mat;
 		}
 
-		Matrix4f Maths::Rotate(Matrix4f& mat, const Quaternion& quat)
+		Matrix4f Rotate(Matrix4f& mat, const Quaternion& quat)
 		{
 			return mat;
 		}
 
-		Matrix4f Maths::Perspective(Matrix4f& mat, float fieldOfView, float aspectRatio, float zNear, float zFar)
+		Matrix4f Perspective(Matrix4f& mat, float fieldOfView, float aspectRatio, float zNear, float zFar)
 		{
 			mat.Identity();
 
@@ -206,7 +262,7 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::Ortho(Matrix4f& mat, float left, float right, float bottom, float top, float near, float far)
+		Matrix4f Ortho(Matrix4f& mat, float left, float right, float bottom, float top, float near, float far)
 		{
 			mat.Identity();
 
@@ -221,7 +277,7 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::LookAt(Matrix4f& mat, const Vector3f& forward, const Vector3f& up, const Vector3f& right)
+		Matrix4f LookAt(Matrix4f& mat, const Vector3f& forward, const Vector3f& up, const Vector3f& right)
 		{
 			mat.Identity();
 
@@ -248,7 +304,7 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::Invers(Matrix4f& mat)
+		Matrix4f Invers(Matrix4f& mat)
 		{
 			float temp[16];
 
@@ -375,7 +431,7 @@ namespace DersEngine
 			return mat;
 		}
 
-		Matrix4f Maths::Transpose(Matrix4f& mat)
+		Matrix4f Transpose(Matrix4f& mat)
 		{
 			return mat;
 		}
--- DersEngine/Source/MathLibrary/Quaternion.cpp
+++ DersEngine/Source/MathLibrary/Quaternion.cpp
@@ -11,6 +11,21 @@ namespace DersEngine
 		Quaternion::Quaternion(float x, float y, float z, float w) 
 			: x(x), y(y), z(z), w(w) {}
 
+
+		Quaternion::Quaternion(const Vector3f& axis, float angle)
+		{
+			float halfAngleInRadians = ToRadians(angle) / 2.0f;
+
+			float sinHalfAngle = Sin(halfAngleInRadians);
+			float cosHalfAngle = Cos(halfAngleInRadians);
+
+			x = axis.x * sinHalfAngle;
+			y = axis.y * sinHalfAngle;
+			z = axis.z * sinHalfAngle;
+
+			w = cosHalfAngle;
+		}
+
 		float Quaternion::Length() const
 		{
 			return sqrt(x * x + y * y + z * z + w * w);
@@ -27,6 +42,11 @@ namespace DersEngine
 			return { -x, -y, -z, w };
 		}
 
+		Quaternion Quaternion::Identity() const
+		{
+			return { 0.0f, 0.0f, 0.0f, 1.0f };
+		}
+
 		Vector3f Quaternion::GetForward() const
 		{
 			return Rotate(Vector3f(0, 0, 1), *this).Normalize();
@@ -40,6 +60,19 @@ namespace DersEngine
 		Vector3f Quaternion::GetRight() const
 		{
 			return Rotate(Vector3f(1, 0, 0), *this).Normalize();
+		}
+
+		std::string Quaternion::ToString() const
+		{
+			std::stringstream result;
+			result << "Quaternion(" << x << ", " << y << ", " << z << ", " << w << ")";
+			return result.str();
+		}
+
+		std::ostream& operator<<(std::ostream& stream, const Quaternion& quat)
+		{
+			stream << quat.ToString();
+			return stream;
 		}
 	}
 }
